9f342ecc3bf55ca0f1020e134c05a6fb09505f1d
Joe Morgan <jmorgan@degdigital.com>
2018-01-04 11:27:08 -0600

    linting #1

--- src/objectUtils.js
+++ src/objectUtils.js
@@ -1,23 +1,24 @@
 import isObject from 'isobject';
 
 export function ensureArray(obj) {
-    if(Array.isArray(obj) === false) {
+    if (Array.isArray(obj) === false) {
         return [obj];
     }
     return obj;
 }
 
 export function assignDeep(target, source) {
-    let output = Object.assign({}, target);
+    const output = Object.assign({}, target);
     if (isObject(target) && isObject(source)) {
       Object.keys(source).forEach(key => {
         if (isObject(source[key])) {
-          if (!(key in target))
-            Object.assign(output, { [key]: source[key] });
-          else
-            output[key] = mergeDeep(target[key], source[key]);
+          if (!(key in target)) {
+Object.assign(output, {[key]: source[key]});
+} else {
+output[key] = mergeDeep(target[key], source[key]);
+}
         } else {
-          Object.assign(output, { [key]: source[key] });
+          Object.assign(output, {[key]: source[key]});
         }
       });
     }
