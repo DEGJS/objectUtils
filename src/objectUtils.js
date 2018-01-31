96673adb75a67519412455cd970a3ae17b9c2462
Joe Morgan <jmorgan@degdigital.com>
2018-01-31 10:24:04 -0600

    clean up styling

--- src/objectUtils.js
+++ src/objectUtils.js
@@ -1,26 +1,26 @@
 import isObject from 'isobject';
 
 export function ensureArray(obj) {
-    if (Array.isArray(obj) === false) {
-        return [obj];
-    }
-    return obj;
+	if (Array.isArray(obj) === false) {
+		return [obj];
+	}
+	return obj;
 }
 
 export function assignDeep(target, source) {
-    const output = Object.assign({}, target);
-    if (isObject(target) && isObject(source)) {
-      Object.keys(source).forEach(key => {
-        if (isObject(source[key])) {
-          if (!(key in target)) {
-Object.assign(output, {[key]: source[key]});
-} else {
-output[key] = mergeDeep(target[key], source[key]);
-}
-        } else {
-          Object.assign(output, {[key]: source[key]});
-        }
-      });
-    }
-    return output;
+	const output = Object.assign({}, target);
+	if (isObject(target) && isObject(source)) {
+		Object.keys(source).forEach(key => {
+			if (isObject(source[key])) {
+				if (!(key in target)) {
+					Object.assign(output, {[key]: source[key]});
+				} else {
+					output[key] = mergeDeep(target[key], source[key]);
+				}
+			} else {
+				Object.assign(output, {[key]: source[key]});
+			}
+		});
+	}
+	return output;
 }
